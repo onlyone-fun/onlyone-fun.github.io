6b74f5d1b3ade4306026b25d5cd2ff532616516d
savior <1460361323@qq.com>
2024-07-09 15:36:09 +0800

    添加api笔记,完善该导航栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,13 +1,14 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  {
-    text:"java-note",
-    icon:"pen-to-square",
-    prefix:"/note/java-note/",
-    children:[
-      { text: "JVM", icon: "pen-to-square", link: "JVM" },
-    ]
-  },
+    "/",
+    {
+        text: "java-note",
+        icon: "pen-to-square",
+        prefix: "/note/java-note/",
+        children: [
+            {text: "JVM", icon: "pen-to-square", link: "JVM"},
+            {text: "API", icon: "pen-to-square", link: "API"},
+        ]
+    },
 ]);
